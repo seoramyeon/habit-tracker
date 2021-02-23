ffd6d9f5b94d485d2a805de373af2cd687762b6c
Nayeon <seoramyeon@gmail.com>
2021-02-23 23:46:30 +0900

    Add button event

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -20,10 +20,16 @@ class Habits extends Component {
 
 	handleDecrement = (habit) => {
 		console.log('handleDecrement', habit.name);
+		const habits = [...this.state.habits];
+		const index = habits.indexOf(habit);
+		const count =  habits[index].count - 1;
+		habits[index].count = count < 0 ? 0 : count;
+		this.setState({ habits: habits});
 	};
 
 	handleDelete = (habit) => {
-		console.log('handleDelete', habit.name);
+		const habits = this.state.habits.filter(item => item.id !== habit.id);
+		this.setState({ habits: habits });
 	};
 
 
